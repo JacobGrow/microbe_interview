a27e9fc19baadbd4f982a1edb8844d9c280443ff
Jacob Grow <jacobagrow@gmail.com>
2020-09-24 20:23:58 -0600

    Model built

---
+++ models/Contact.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Contact = db.define('contact', {
+  name: {
+    type: Sequelize.STRING
+  },
+  role: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  },
+})
+
+module.exports = Contact;
